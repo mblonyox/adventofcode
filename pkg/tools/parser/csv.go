5ba856f0370d0a3c1e11fc45c326a31212547908
mblonyox <mblonyox@gmail.com>
2019-12-08 10:49:20 +0700

    Add functionality to csv parser

--- pkg/tools/parser/csv.go
+++ pkg/tools/parser/csv.go
@@ -4,20 +4,21 @@ import (
 	"encoding/csv"
 	"os"
 	"strconv"
+	"strings"
 )
 
 // ParseIntCsv parse the input.txt into slice of int
 func ParseIntCsv() (result []int, err error) {
-	input, err := os.Open("input.txt")
+	record, err := ParseCsvInput()
 	if err != nil {
 		return
 	}
-	defer input.Close()
-	csvReader := csv.NewReader(input)
-	record, err := csvReader.Read()
-	if err != nil {
-		return
-	}
+	result, err = MapAtoi(record)
+	return
+}
+
+//MapAtoi map the strconv.Atoi function to slice of strings
+func MapAtoi(record []string) (result []int, err error) {
 	result = make([]int, len(record))
 	for i, s := range record {
 		result[i], err = strconv.Atoi(s)
@@ -25,5 +26,24 @@ func ParseIntCsv() (result []int, err error) {
 			return
 		}
 	}
+	return
+}
+
+// ParseCsvInput parse the input.txt into slice of string
+func ParseCsvInput() (result []string, err error) {
+	input, err := os.Open("input.txt")
+	if err != nil {
+		return
+	}
+	defer input.Close()
+	csvReader := csv.NewReader(input)
+	result, err = csvReader.Read()
+	return
+}
+
+// ParseCsvString parse the string into slice of string
+func ParseCsvString(s string) (result []string, err error) {
+	csvReader := csv.NewReader(strings.NewReader(s))
+	result, err = csvReader.Read()
 	return
 }
