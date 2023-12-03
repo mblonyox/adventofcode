6ad29c3cca893959f3cd0205d3e334307e92880f
Sukirno <mblonyox@gmail.com>
2023-12-03 23:56:47 +0700

    wip: day3 2023

---
+++ 2023/3/part2.exs
@@ -0,0 +1,39 @@
+input = Enum.at(System.argv(), 0) || "input.txt"
+
+parse_line = fn {line, index} ->
+  Regex.scan(~r/\d+|[^\d\.]/, line, return: :index)
+  |> Enum.map(&Kernel.hd/1)
+  |> Enum.map(fn {start, length} ->
+    text = String.slice(line, start, length)
+
+    Integer.parse(text)
+    |> case do
+      {number, _remainder} -> {number, index, start, length}
+      :error -> {text, index, start, length}
+    end
+  end)
+end
+
+process_numbers = fn %{numbers: numbers, symbols: symbols} ->
+  Enum.filter(symbols, &(elem(&1, 0) == "*"))
+  |> Enum.map(fn {_, symbol_index, symbol_start, _} ->
+    Enum.filter(numbers, fn {_, number_index, number_start, length} ->
+      Enum.member?((number_index - 1)..(number_index + 1), symbol_index) &&
+        Enum.member?((number_start - 1)..(number_start + length), symbol_start)
+    end)
+    |> Enum.map(&elem(&1, 0))
+  end)
+  |> Enum.filter(&(length(&1) == 2))
+  |> Enum.map(&Enum.product/1)
+  |> Enum.sum()
+end
+
+result =
+  File.stream!(input)
+  |> Stream.map(&String.trim/1)
+  |> Stream.with_index()
+  |> Stream.flat_map(parse_line)
+  |> Enum.group_by(&((is_integer(elem(&1, 0)) && :numbers) || :symbols))
+  |> process_numbers.()
+
+IO.inspect(result, label: "Result")
