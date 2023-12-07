7f189da16bda6dd8a57d35cc8222c87a347f9631
Sukirno <sukirno89@kemenkeu.go.id>
2023-12-07 19:44:30 +0700

    done: day 7 2023

---
+++ 2023/7/part2.exs
@@ -0,0 +1,63 @@
+parse_line = fn line ->
+  [hand, bid] = line |> String.trim() |> String.split(" ")
+  {hand, bid |> String.to_integer()}
+end
+
+strength = %{
+  five_of_a_kind: 6,
+  four_of_a_kind: 5,
+  full_house: 4,
+  three_of_a_kind: 3,
+  two_pair: 2,
+  one_pair: 1,
+  high_card: 0
+}
+
+hand_type = fn hand ->
+  hand
+  |> String.split("", trim: true)
+  |> Enum.reduce(%{}, fn c, acc ->
+    acc
+    |> Map.update(c, 1, &(&1 + 1))
+  end)
+  |> Map.pop("J", 0)
+  |> then(fn {j, count} ->
+    Map.values(count)
+    |> Enum.sort(:desc)
+    |> case do
+      [2, 2, 1] ->
+        :two_pair
+
+      [n, 2] when n + j == 3 ->
+        :full_house
+
+      [n | _] ->
+        case n + j do
+          5 -> :five_of_a_kind
+          4 -> :four_of_a_kind
+          3 -> :three_of_a_kind
+          2 -> :one_pair
+          1 -> :high_card
+        end
+
+      [] ->
+        :five_of_a_kind
+    end
+  end)
+end
+
+__ENV__.file
+|> Aoc2023.input_stream()
+|> Stream.map(parse_line)
+|> Enum.sort_by(
+  &{strength[&1 |> elem(0) |> then(hand_type)],
+   &1
+   |> elem(0)
+   |> String.replace("J", "1")
+   |> String.replace("T", ":")
+   |> String.replace("K", "k")
+   |> String.replace("A", "Ã")}
+)
+|> Enum.with_index(&(elem(&1, 1) * (&2 + 1)))
+|> Enum.sum()
+|> IO.inspect(label: "Result")
