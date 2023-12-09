d418bdab8a841a4e81c83248fe526e18e4688fb9
Sukirno <mblonyox@gmail.com>
2023-12-09 15:02:30 +0700

    day 9 2023

---
+++ 2023/9/part1.exs
@@ -0,0 +1,22 @@
+parse_input = fn line ->
+  line
+  |> String.split(~r/\s+/, trim: true)
+  |> Enum.map(&String.to_integer/1)
+end
+
+predict_next_value = fn numbers ->
+  numbers
+  |> Enum.reduce([], fn n, acc ->
+    acc
+    |> Enum.map_reduce(n, fn h, n -> {n, n - h} end)
+    |> then(fn {h, n} -> Enum.concat(h, [n]) end)
+  end)
+  |> Enum.sum()
+end
+
+__ENV__.file
+|> Aoc2023.input_stream()
+|> Stream.map(parse_input)
+|> Stream.map(predict_next_value)
+|> Enum.sum()
+|> IO.inspect(label: "Result")
